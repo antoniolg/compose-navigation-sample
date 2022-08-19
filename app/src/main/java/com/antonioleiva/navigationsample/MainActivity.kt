2a4eac125265b96197f6c14ede6d2e38217fd0f6
antoniolg <antonio@devexperto.com>
2022-08-19 10:25:03 +0200

    Simple navigation

--- app/src/main/java/com/antonioleiva/navigationsample/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/navigationsample/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.antonioleiva.navigationsample.ui.Content
 import com.antonioleiva.navigationsample.ui.Home
 import com.antonioleiva.navigationsample.ui.theme.NavigationSampleTheme
 
@@ -15,8 +19,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             NavigationSampleTheme {
-                Home()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        Home(onNavigate = { navController.navigate("detail") })
+                    }
+                    composable("detail") {
+                        Content(text = "Detail")
+                    }
+                }
             }
         }
     }
